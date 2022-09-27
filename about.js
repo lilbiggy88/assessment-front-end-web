d369cc7b40a1d4a872f39b1c31ed6e3fd9013086
John Biggerstaff <john.r.biggerstaff@gmail.com>
2022-09-26 22:14:10 -0600

    part 2 done

--- about.js
+++ about.js
@@ -5,10 +5,17 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('The form has been submitted!');
 }
 
+function rubberDuck(evt) {
+	evt.preventDefault();
+
+	alert("You found my rubber ducky! RUBBER DUCKY! YOU'RE THE ONE!")
+}
 
 let form = document.querySelector('#contact');
-
+let duckPic = document.querySelector('#rubber-ducky')
 form.addEventListener('submit', handleSubmit);
+duckPic.addEventListener('mouseover', rubberDuck);
+
